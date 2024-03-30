0acf85fafc61104914002df97465997a3e01b34a
frenchcokes <144563579+frenchcokes@users.noreply.github.com>
2024-03-30 15:36:38 -0600

    pt 4 update

--- project/polls/views.py
+++ project/polls/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.db.models import F
 
 from .models import Choice, Question
 
